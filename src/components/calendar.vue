d08dc7be94b5de0bdd7e18f0c48c65df56fa52f1
William <awaitfornemesis@gmail.com>
2019-06-17 12:25:50 +0500

    new calendar

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -7,12 +7,14 @@
         </div>
         <div class="info">
             <div class="info__calendar">
-                <v-date-picker
-                        @input="changeData"
-                        mode='single'
-                        v-model='selectedDate'
-                        show-caps>
-                </v-date-picker>
+                <functional-calendar
+                        :is-date-picker="true"
+                        :date-format="'dd.mm.yyyy'"
+                        :day-names="['П', 'В', 'С', 'Ч', 'П', 'С', 'В']"
+                        :monthNames="['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']"
+                        v-model="calendarData"
+                        @input="changeData">
+                </functional-calendar>
             </div>
             <schedule></schedule>
         </div>
@@ -20,22 +22,14 @@
 </template>
 
 <script>
-    import {setupCalendar, Calendar, DatePicker} from 'v-calendar';
-    import 'v-calendar/lib/v-calendar.min.css';
     import schedule from '../components/schedule';
-
-    setupCalendar({
-        firstDayOfWeek: 2,
-        popoverVisibility: "visible",
-        popoverExpanded: true
-    });
+    import {FunctionalCalendar} from 'vue-functional-calendar';
 
     export default {
         name: "calendar",
         components: {
-            'v-calendar': Calendar,
-            'v-date-picker': DatePicker,
-            schedule
+            schedule,
+            FunctionalCalendar
         },
         data: () => {
             return {
@@ -49,7 +43,13 @@
                         dates: new Date()
                     }
                 ],
-                selectedDate: new Date(),
+                calendarData: {},
+                calendarConfigs: {
+                    isDatePicker: true,
+                    dateFormat: 'dd.mm.yyyy',
+                    dayNames: ['П', 'В', 'С', 'Ч', 'П', 'С', 'В'],
+                    monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+                }
             };
         },
         computed: {
@@ -58,24 +58,20 @@
             },
         },
         methods: {
-            changeTabs: function (e,tabsID) {
+            changeTabs: function (e, tabsID) {
                 this.$store.dispatch('changeTabs', tabsID);
 
-                let arr =  document.body.querySelectorAll('[data-active]');
-                arr.forEach(function (item, i, arr ) {
+                let arr = document.body.querySelectorAll('[data-active]');
+                arr.forEach(function (item, i, arr) {
                     item.classList.remove('active')
                 });
-                if (!(e.target.getAttribute('class').includes('active'))){
-                     e.target.className += ' active'
+                if (!(e.target.getAttribute('class').includes('active'))) {
+                    e.target.className += ' active'
                 }
             },
             changeData() {
-                let dataDate = this.selectedDate.getDate().toString();
-                let dataMonth = this.selectedDate.getMonth()+1;
-                dataMonth.toString();
-                let dataYear = this.selectedDate.getFullYear().toString();
-                let dataFull = dataDate+'.'+dataMonth+'.'+dataYear;
-                this.$store.dispatch('changeData', dataFull)
+                let dataSelected = this.calendarData.selectedDate;
+                this.$store.dispatch('changeData', dataSelected)
             },
         }
     }
@@ -85,18 +81,17 @@
     .container {
         display: block;
         width: 1366px;
+        height: 100%;
         margin: 0 auto;
     }
 
     .info {
+        flex: 1 1 100%;
         display: flex;
     }
 
     .info__calendar {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        flex: 0 0 300px;
+        width: 300px;
     }
 
     .tabs {
@@ -116,5 +111,8 @@
                 background: #e4e4e4;
             }
         }
+    }
+    .calendar-wrap {
+        width: 300px;
     }
 </style>
